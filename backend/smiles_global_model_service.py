e0c796c6c4964cca4c889ab86f040ad6749892fb
dionizijefa <dionizije.fa@hotmail.com>
2021-07-26 12:28:12 +0200

    Fix transformer inference

--- backend/smiles_global_model_service.py
+++ backend/smiles_global_model_service.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from inference_model import TransformerNet
 from inference_model import process_molecule
+import numpy as np
 
 # declare constants
 HOST = '0.0.0.0'
@@ -15,18 +16,23 @@ class Inference:
     def __init__(self, ckpt_path):
         self.model = TransformerNet.load_from_checkpoint(ckpt_path)
 
-    def forward(self):
-        out = self.model.forward()
+    def forward(self, node, adj, dist, mask):
+        out = self.model.forward(node, adj, dist, mask, None)
         out = out.squeeze(-1).detach().cpu().numpy()
+        return out
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
     input_smiles = request.get_json()['smilesInput']
     node, adj, dist, mask = process_molecule(input_smiles)
-    print(node)
 
-    return jsonify({'predictedProbability': atoms})
+    model = Inference('epoch=6-step=405.ckpt')
+    logit = model.forward(node, adj, dist, mask)
+    odds = np.exp(logit)
+    probability = odds / (1+odds)
+
+    return jsonify({'predictedProbability': probability})
 
 
 if __name__ == '__main__':
